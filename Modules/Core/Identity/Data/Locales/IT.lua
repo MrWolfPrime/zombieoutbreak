2a4db8b7cbca331b9f65454be5b9986b327b298c
MrWolfPrime <73306500+MrWolfPrime@users.noreply.github.com>
2021-01-03 14:48:18 +0100

    Add files via upload

---
+++ Modules/Core/Identity/Data/Locales/IT.lua
@@ -0,0 +1,8 @@
+Translations = {
+    ["Unemployed"] = "Sopravvisuto",
+    ["Unemployed_Description"] = "Storia Sopravvissuto",
+    ["Police"] = "Agente di polizia",
+    ["Police_Description"] = "Storia Agente",
+    ["Paramedic"] = "Paramedico",
+    ["Paramedic_Description"] = "Storia paramedico"
+}
