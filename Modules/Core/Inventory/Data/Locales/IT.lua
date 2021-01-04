76809797bd7bc6112de2d8a0ea471b72715a391b
MrWolfPrime <73306500+MrWolfPrime@users.noreply.github.com>
2021-01-04 15:13:40 +0100

    Inventory in Italian

---
+++ Modules/Core/Inventory/Data/Locales/IT.lua
@@ -0,0 +1,21 @@
+    Translations = {
+        ["No_Player_Nearby"] = "~b~[Info]~s~ Non ci sono giocatori vicino.",
+        ["Cant_Carry_Item"] = "~y~[ATTENZIONE]~s~ Limite inventario massimo raggiunto .",
+        ["Cant_Carry_Weapon"] = "~y~[ATTENZIONE]~s~ Possiedi già quest'arma.",
+        ["Encumbered"] = "~y~[ATTENZIONE]~s~ Peso massimo raggiunto, Non puoi trasportare altro.",
+        ["Player_Give_Encumbered"] = "~y~[ATTENZIONE]~s~ Stai provando a passare ~y~%s~s~ a ~b~%s~s~, ma il suo inventario è pieno.",
+        ["Target_Give_Encumbered"] = "~y~[ATTENZIONE]~s~ ~b~%s~s~ sta provando a passarti ~y~%s~s~, Ma il tuo inventario è pieno.",
+        ["Player_Give_OneItem"] = "~b~[Info]~s~ Hai passato ~y~%s~s~ a ~b~%s~s~.",
+        ["Target_Give_OneItem"] = "~b~[Info]~s~ Hai ricevuto ~y~%s~s~ da ~b~%s~s~.",
+        ["Player_Give_Item"] = "~b~[Info]~s~ Hai passato ~y~%s~s~ x%s a ~b~%s~s~.",
+        ["Target_Give_Item"] = "~b~[Info]~s~ Hai ricevuto ~y~%s~s~ x%s da ~b~%s~s~.",
+        ["Player_Give_CantTakeMore"] = "~y~[ATTENZIONE]~s~ Stai provando a passare ~y~%s~s~ a ~b~%s~s~, ma il suo inventario è pieno.",
+        ["Target_Give_CantTakeMore"] = "~y~[ATTENZIONE]~s~ ~b~%s~s~ sta provando a passarti ~y~%s~s~, ma il tuo inventario è pieno.",
+        ["Player_Give_Weapon"] = "~b~[Info]~s~ Hai passato ~y~%s~s~ a ~b~%s~s~.",
+        ["Target_Give_Weapon"] = "~b~[Info]~s~ Hai ricevuto ~y~%s~s~ da ~b~%s~s~.",
+        ["Player_Give_HasWeapon"] = "~y~[Info]~s~ Stai provando a passare ~y~%s~s~ a ~b~%s~s~, ma già ne possiede una.",
+        ["Target_Give_HasWeapon"] = "~y~[ATTENZIONE]~s~ ~b~%s~s~ sta provando a passarti ~y~%s~s~, ma ne possiedi già una.",
+        ["Clothes_Not_Available"] = "~y~[ATTENZIONE]~s~ ~y~%s~s~ non è della tua taglia.",
+        ["Clothes_Not_Available_For_Player_Sex"] = "~y~[ATTENZIONE]~s~ ~y~%s~s~ Non è del tuo sesso."
+
+    }
